4f181fae60c90a266eeebe2ee7cc396a63f4275d
Marassanovad <maradasha@mail.ru>
2022-09-24 22:12:51 +0600

    Change HW

--- Lesson_1/HW_Task_2.py
+++ Lesson_1/HW_Task_2.py
@@ -1,16 +1,18 @@
 # 2. Напишите программу для. проверки истинности утверждения
 #  ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
 
-x = int(input('Введите x: '))
-y = int(input('Введите y: '))
-z = int(input('Введите z: '))
+# x = int(input('Введите x: '))
+# y = int(input('Введите y: '))
+# z = int(input('Введите z: '))
+for x in range(2):
+    for y in range(2):
+        for z in range(2):
+            if (not (x or y or z) == (not x and not y and not z)):
+                print(x, y, z)
+                print("Утверждение истинно")
+            else:
+                print(x, y, z)
+                print("Утверждение ложно")
 
 
-leftside = not (x or y or z)
-rightside = not x and not y and not z
 
-
-if leftside == rightside:
-    print("Утверждение истинно")
-else:
-    print("Утверждение ложно")
