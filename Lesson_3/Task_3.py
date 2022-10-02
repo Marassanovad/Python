434868cf624106bc6e123d09512e29ec89226396
Marassanovad <maradasha@mail.ru>
2022-10-02 11:49:37 +0600

    Create file Lesson_4

--- Lesson_3/Task_3.py
+++ Lesson_3/Task_3.py
@@ -1,26 +1,16 @@
-def regres(a,b):
-    result = []
-    while a != 0:
-        result.append(a % b)
-        a = a//b
-    result.reverse()
-    return result
-num = 11
-razraydnost = 2
-print(*regres(num, razraydnost))
+# Mimi calculator
 
 
+num1 = int(input("Enter num1: "))
+num2 = int(input("Enter num2: "))
+action = str(input("Choose action: Add(a), Sub(S) Mult(m) Div(d) ->"))
 
-# num1 = int(input("Enter num1: "))
-# num2 = int(input("Enter num2: "))
-# action = str(input(*Choose action: Add(a), Sub(S)
-# Mult(m) Div(d) ->"))
-# print("The result is "end=*)
-# If action == "a";
-# print(num1+num2)
-# elif action == 's":
-# print(num1-num2)
-# elif action == "m':
-# print(num1*num2)
-# else:
-# print(num1/num2)
+print("The result is ", end="")
+if action == "a":
+    print(num1+num2)
+elif action == "s":
+    print(num1-num2)
+elif action == "m":
+    print(num1*num2)
+else:
+    print(num1/num2)
